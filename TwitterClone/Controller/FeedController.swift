dc9d060a3dcd97d6c0f36bf8a02853dd5b65797a
karinkatrich <karinkatrich@gmail.com>
2020-05-21 17:44:40 +0200

    Structure User Feed

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -38,22 +38,37 @@ class FeedController: UICollectionViewController {
         navigationController?.navigationBar.barStyle = .default
     }
 
+    //MARK: - Selectors
+
+    @objc func handleRefresh() {
+        fetchTweets()
+    }
+
     //MARK: - API
 
     func fetchTweets() {
-        TweetService.shared.fetchTweets { (tweets) in
-            self.tweets = tweets
-            self.checkIfUserLikedTweets(tweets)
+        collectionView.refreshControl?.beginRefreshing()
+        
+        TweetService.shared.fetchTweets { tweets in
+            self.tweets = tweets.sorted(by: { $0.timestamp > $1.timestamp })
+            self.checkIfUserLikedTweets()
+
+
+            self.collectionView.refreshControl?.endRefreshing()
         }
     }
 
     //MARK: - Helpers
 
-    func checkIfUserLikedTweets(_ tweets: [Tweet]) {
-        for (index, tweet) in tweets.enumerated() {
+    func checkIfUserLikedTweets() {
+        self.tweets.forEach { tweet in
             TweetService.shared.checkIfUserLikedTweet(tweet) { didLike in
                 guard didLike == true else { return }
-                self.tweets[index].didLike = true
+
+
+                if let index = self.tweets.firstIndex(where: { $0.tweetID == tweet.tweetID }) {
+                    self.tweets[index].didLike = true
+                }
             }
         }
     }
@@ -67,6 +82,10 @@ class FeedController: UICollectionViewController {
         imageView.contentMode  = .scaleAspectFill
         imageView.setDimensions(width: 44, height: 44)
         navigationItem.titleView = imageView
+
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
     }
 
     func configureLeftBarButton() {
